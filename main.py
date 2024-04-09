500291204f4ea867a06148dabfbd94415d5df04a
Akma <akosya.akmaral@gmail.com>
2024-04-09 20:53:16 +0500

    added to main.py changes to host

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ if response.status_code == 200:
 
 
     # file_path = "../../index.html" 
-    file_path = "modified.html"
+    file_path = "index.html"
 
     try:
         with open(file_path, "w", encoding="utf-8") as file:
